636964661b2229248c817b229df6aeb3962dc210
shahriyardx <mdshahriyaralam552@gmail.com>
2024-08-31 18:30:38 +0600

    Updated module docs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 /**
- * Entry point for the WebSocket client library.
- * Re-exports all necessary modules from the client and types files.
+ * WebSocket client library to communicate with Winerp server.
  * @module
+ * @example
+ * ```ts
+ * import { Client } from "@shahriyarx/winerp-client-node"
+ * 
+ * client = new Client({ host: "localhost", port: 2033, local_name: "dashboard" })
+ * 
+ * // Making request
+ * await client.request({ destination: "bot", route: "guild_roles", data: { guild_id: 123 } })
  */
 export * from "./client";
 export * from "./types";
